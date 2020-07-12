ab4bba9ee3d3e69ab607e0e2663b010fc9f36d1c
Alexandar <alexandar1000@gmail.com>
2020-07-12 21:23:38 +0100

    Add the TestScript

---
+++ Assets/RosSharp/Scripts/RosBridgeClient/RosCommuncation/TestScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RosSharp.RosBridgeClient
+{
+    public class TestScript : UnitySubscriber<MessageTypes.Std.String>
+    {
+        public int counter = 0;
+
+        protected override void Start()
+        {
+            base.Start();
+        }
+        
+        protected override void ReceiveMessage(MessageTypes.Std.String msg)
+        {
+            counter++;
+
+            Debug.Log("Message " + counter++ + ": " + msg.data);
+        }
+    }
+}
