422e811042746009f53de5e18baae67b99cca37b
Atonal <1048904413@qq.com>
2020-07-28 20:09:57 +0100

    Send and receive battery power data from Unity to ROS successfully

---
+++ Assets/Scripts/Battery/Battery.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Battery : MonoBehaviour
+{
+    private const int MAX_POWER = 100;
+    private const int MIN_POWER = 0;
+    public static float power;
+    public float consumeRate = 1f;
+    public bool boatCanMove;
+    private int boatStatus = 0;     
+
+    /* public bool isCharging;
+     public int chargeRate = 3;
+     private float chargeStart;
+     private float chargeEnd;*/
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject battery = GameObject.Find("Battery");
+        power = MAX_POWER;
+        UnityEngine.Debug.Log("Battery start, and power is " + power);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //update boat status
+        if (power > 0)
+        {
+            boatStatus = 0;
+            power = MAX_POWER - Time.time * consumeRate; //Time.time: number of seconds from the start of game
+        }
+        else if (boatStatus <= 1)
+        {
+            boatStatus++;
+        }
+
+        //boat stop condition
+        if (boatStatus == 1)
+        {
+            UnityEngine.Debug.Log("The battery runs out off power, boat stopped.");
+        }
+        if (boatStatus == 0)
+        {
+            boatCanMove = true;
+        }
+        else
+        {
+            boatCanMove = false;
+        }
+
+        //battery recharge
+        /*if (isCharging)
+       {
+
+       }*/
+
+    }
+}
