4470c7f2756ca45e04462f99a6a8a19dd6e6b309
Aleksandar <alexandar1000@gmail.com>
2020-08-04 00:29:34 +0100

    Move the project from the forked branch

---
+++ Assets/MayFlower/Scripts/PlayerDemp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDemp : MonoBehaviour
+{
+ public int maxPower = 100;
+ public int currentPower;
+public Battery battery;
+
+ void Start() {
+     currentPower = maxPower;
+     battery.setPower(currentPower);
+     
+ }
+ private void Update() {
+     
+     currentPower -= (int)Time.deltaTime;
+     battery.setPower(currentPower);
+
+     
+ }
+}
