90f459d331b4640bc5bd215dc3a94505522c15c4
Atonal <1048904413@qq.com>
2020-08-05 11:18:52 +0100

    Revert "check for saving"
    This reverts commit 900efac010bc4a78dd5d6592cbecb86712dcbe9e.

---
+++ Assets/MayFlower/Scripts/Battery.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Battery : MonoBehaviour
+{
+    public Slider slider; 
+    public Gradient gradient;
+    public Image fill;
+
+
+
+    public void setMaxPower(int power){
+        slider.maxValue = power;
+        slider.value =  power;
+        // fill.color = gradient.Evaluate(1f);
+    }
+    public void setPower(int power){
+        slider.value = power;
+ 
+    }
+
+}
