ff12ec738f286d6e2a2b951e7592bdde03c0c2aa
aunroel <Ytxsgfq123yjen>
2020-08-05 23:56:04 +0100

    added video camera sensor, attached it to the front of the boat. Images are published to ROS topic every second. Main camera now follows the boat.

---
+++ Assets/Scripts/EnvironmentData/VideoFeed/CameraFollow.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject boat;
+
+    public Transform target;
+    private Vector3 offset;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - boat.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = boat.transform.position + offset;
+    }
+}
