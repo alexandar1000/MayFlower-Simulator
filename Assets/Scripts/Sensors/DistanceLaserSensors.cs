f6d4dcd8dfe9a4d03f66b6d80c8df6d704e5ffaa
najatbaqadir <najat.m7md@hotmail.com>
2020-08-09 20:56:02 +0300

    merge with nb

---
+++ Assets/Scripts/Sensors/DistanceLaserSensors.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace RosSharp.RosBridgeClient
+{
+   public class DistanceLaserSensors : UnityPublisher<MessageTypes.Std.Float64>
+   {
+
+      public GameObject boat;
+      public GameObject canal;
+      public float distance;
+
+
+      // Start is called before the first frame update
+
+      [Header("Sensors")]
+      public float sensorLength = 5f;
+      public Vector3 frontSensorPosition = new Vector3(0f,0.2f,0.5f);
+      public float frontSideSensorPosition = 0.2f;
+      public float frontSensorAngle = 30;
+      public float backSensorPosition = 0.2f;
+
+
+      // Update is called once per frame
+      void Update()
+      {
+         distance = Vector3.Distance(boat.transform.position,canal.transform.position);
+         if (distance < 3.5)
+         {
+               Debug.Log("ALERT: BOAT CLOSER TO CANAL");
+         }
+         laserSensors();
+      }
+
+      private void laserSensors(){
+            RaycastHit hit;
+            Vector3 sensorStartPos = transform.position + frontSensorPosition;
+
+            
+            //Front Centre Sensor
+            if(Physics.Raycast(sensorStartPos, transform.forward, out hit, sensorLength)){
+               Debug.DrawLine(sensorStartPos, hit.point);
+
+            }
+
+            //Front Right Side Sensor
+            sensorStartPos.x += frontSideSensorPosition;
+            if(Physics.Raycast(sensorStartPos, transform.forward, out hit, sensorLength)){
+               Debug.DrawLine(sensorStartPos, hit.point);
+            }
+            
+
+            //Front Right Angle Sensor
+            if(Physics.Raycast(sensorStartPos, Quaternion.AngleAxis(frontSensorAngle, transform.up) * transform.forward, out hit, sensorLength)){
+               Debug.DrawLine(sensorStartPos, hit.point);
+            }
+
+            //Front Left Side Sensor
+            sensorStartPos.x -= 2 * frontSideSensorPosition;
+            if(Physics.Raycast(sensorStartPos, transform.forward, out hit, sensorLength)){
+               Debug.DrawLine(sensorStartPos, hit.point);
+            }
+
+            //Front Left Angle Sensor
+            if(Physics.Raycast(sensorStartPos, Quaternion.AngleAxis(-frontSensorAngle, transform.up) * transform.forward, out hit, sensorLength)){
+               Debug.DrawLine(sensorStartPos, hit.point);
+            }
+
+            //Back Sensor
+            sensorStartPos.y += backSensorPosition;
+            if(Physics.Raycast(sensorStartPos, transform.forward * -1, out hit, sensorLength)){
+               Debug.DrawLine(sensorStartPos, hit.point);
+
+            }
+            
+         Debug.Log(distance);
+         Publish(PrepareMessage(distance));
+            
+      }
+   private MessageTypes.Std.Float64 PrepareMessage(float laser)
+   {
+    MessageTypes.Std.Float64 message = new MessageTypes.Std.Float64();
+    message.data = laser;
+
+    return message;
+   }
+   }
+}
