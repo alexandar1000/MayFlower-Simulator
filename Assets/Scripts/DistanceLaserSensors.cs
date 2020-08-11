cc013930f94d83d8c9a0dca97b23c33bece6bdd3
sbs61@hi.is <sbs61@hi.is>
2020-08-11 12:09:15 +0000

    update

--- Assets/Scripts/DistanceLaserSensors.cs
+++ Assets/Scripts/DistanceLaserSensors.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 
 namespace RosSharp.RosBridgeClient
 {
-   public class DistanceLaserSensors : UnityPublisher<MessageTypes.Std.Float64>
+   public class DistanceLaserSensors : UnityPublisher<MessageTypes.Std.Int64MultiArray>
    {
 
       public GameObject boat;
@@ -23,28 +24,45 @@ namespace RosSharp.RosBridgeClient
       public float backSensorPosition = 0.2f;
       private float sensorNr = 0;
 
+       private float nextActionTime = 0.0f;
+      public float period = 0.1f;
+
+/*
+      void Start(){
+         InvokeRepeating("laserSensors", 0.5f, 0.1f);
+      }
+      */
 
       // Update is called once per frame
       void Update()
       {
+
+         if (Time.time > nextActionTime ) {
+        nextActionTime += period;
+        
          distance = Vector3.Distance(boat.transform.position,canal.transform.position);
-         Debug.Log(sensorNr);
+         //Debug.Log(sensorNr);
          if (distance < 3.5)
          {
                Debug.Log("ALERT: BOAT CLOSER TO CANAL");
          }
          laserSensors();
+     }
+
       }
 
       private void laserSensors(){
             RaycastHit hit;
             Vector3 sensorStartPos = transform.position + frontSensorPosition;
 
+            long[] sensors = {0,0,0,0,0,0};
+
             
             //Front Centre Sensor
             if(Physics.Raycast(sensorStartPos, transform.forward, out hit, sensorLength)){
                Debug.DrawLine(sensorStartPos, hit.point);
                sensorNr = 1;
+               sensors[0] = 1;
             }
 
             //Front Right Side Sensor
@@ -52,13 +70,15 @@ namespace RosSharp.RosBridgeClient
             if(Physics.Raycast(sensorStartPos, transform.forward, out hit, sensorLength)){
                Debug.DrawLine(sensorStartPos, hit.point);
                sensorNr = 2;
+               sensors[1] = 1;
             }
             
 
             //Front Right Angle Sensor
-            if(Physics.Raycast(sensorStartPos, Quaternion.AngleAxis(frontSensorAngle, transform.up) * transform.forward, out hit, sensorLength)){
+            else if(Physics.Raycast(sensorStartPos, Quaternion.AngleAxis(frontSensorAngle, transform.up) * transform.forward, out hit, sensorLength)){
                Debug.DrawLine(sensorStartPos, hit.point);
                sensorNr = 3;
+               sensors[2] = 1;
             }
 
             //Front Left Side Sensor
@@ -66,12 +86,14 @@ namespace RosSharp.RosBridgeClient
             if(Physics.Raycast(sensorStartPos, transform.forward, out hit, sensorLength)){
                Debug.DrawLine(sensorStartPos, hit.point);
                sensorNr = 4;
+               sensors[3] = 1;
             }
 
             //Front Left Angle Sensor
-            if(Physics.Raycast(sensorStartPos, Quaternion.AngleAxis(-frontSensorAngle, transform.up) * transform.forward, out hit, sensorLength)){
+            else if(Physics.Raycast(sensorStartPos, Quaternion.AngleAxis(-frontSensorAngle, transform.up) * transform.forward, out hit, sensorLength)){
                Debug.DrawLine(sensorStartPos, hit.point);
                sensorNr = 5;
+               sensors[4] = 1;
             }
 
             //Back Sensor
@@ -79,16 +101,19 @@ namespace RosSharp.RosBridgeClient
             if(Physics.Raycast(sensorStartPos, transform.forward * -1, out hit, sensorLength)){
                Debug.DrawLine(sensorStartPos, hit.point);
                sensorNr = 6;
+               sensors[5] = 1;
             }
+
+         Debug.Log(sensors[0] + ", " + sensors[1] + ", " + sensors[2] + ", " + sensors[3] + ", " + sensors[4] + ", " + sensors[5]);
             
-         Debug.Log(distance);
-         Publish(PrepareMessage(sensorNr));
+         //Debug.Log(distance);
+         Publish(PrepareMessage(sensors));
             
       }
-   private MessageTypes.Std.Float64 PrepareMessage(float laser)
+   private MessageTypes.Std.Int64MultiArray PrepareMessage(long[] msg)
    {
-    MessageTypes.Std.Float64 message = new MessageTypes.Std.Float64();
-    message.data = laser;
+    MessageTypes.Std.Int64MultiArray message = new MessageTypes.Std.Int64MultiArray();
+    message.data = msg;
 
     return message;
    }
