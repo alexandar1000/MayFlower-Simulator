562c5f987e8cc6808fd8842ae7147aa034026b5e
Alexandar <alexandar1000@gmail.com>
2020-08-11 08:16:38 +0100

    Split Lidar into 2D and 3D and make base class abstract

--- Assets/Scripts/Sensors/LiDAR/LiDAR.cs
+++ Assets/Scripts/Sensors/LiDAR/LiDAR.cs
@@ -4,81 +4,39 @@ using UnityEngine;
 
 namespace RosSharp.RosBridgeClient
 {
-    public class LiDAR : UnityPublisher<MessageTypes.Sensor.LaserScan>
-    {
-        private Laser _laser; 
-        private RotationScan _rotationScan;
+    public abstract class LiDAR : UnityPublisher<MessageTypes.Sensor.LaserScan>
+    { 
         public float LaserLength;
         public float RotationPerMinute;
         public int ScanningFrequency;
-        private float _rotationStep;
-        private float _rotationDuratuion;
-        private Vector3 _rotationAxis;
-        private int _scansPerRotation;
+        protected float RotationStep;
+        protected float RotationDuratuion;
+        protected Vector3 RotationAxis;
+        protected int ScansPerRotation;
         public bool ShowLasers = true;
         public string FrameId = "Unity";
-        private MessageTypes.Sensor.LaserScan message;
-        Timer myTimer;
-        int scanCounter = 0;
-        private Vector3 _boatDirection;
-
-        private Vector3 _initialAngle;
+        protected MessageTypes.Sensor.LaserScan Message;
+        protected Vector3 BoatDirection;
+        protected Vector3 InitialAngle;
 
         // Start is called before the first frame update
         protected override void Start()
         {
             base.Start();
 
-            _rotationStep = RotationPerMinute / 60;
-            _rotationDuratuion = 1 / _rotationStep;
-            _rotationAxis = transform.up;
-            _scansPerRotation = (int) (_rotationDuratuion * ScanningFrequency);
-            _rotationScan = new RotationScan(_scansPerRotation, LaserLength);
-            _initialAngle = transform.forward;
-            _boatDirection = transform.parent.forward;
-            InitializeMessage();
-            InvokeRepeating("UpdateMessage", 1f, 1f);
+            RotationStep = RotationPerMinute / 60;
+            RotationDuratuion = 1 / RotationStep;
+            RotationAxis = transform.up;
+            ScansPerRotation = (int) (RotationDuratuion * ScanningFrequency);
+            InitialAngle = transform.forward;
+            BoatDirection = transform.parent.forward;
         }
 
         // Update is called once per frame
         void Update()
         {
-            transform.Rotate(0, _rotationStep * 360 * Time.deltaTime, 0);
-            _boatDirection = transform.parent.forward;
-        }
-
-        private void InitializeMessage()
-        {
-            message = new MessageTypes.Sensor.LaserScan();
-            message.header.frame_id = FrameId;
-            message.angle_min = 0f;
-            message.angle_max = 6.28f; // Full circle/rotation
-            message.angle_increment = 6.28f / _scansPerRotation;
-            message.time_increment = 0;
-            message.scan_time = 0.1f;
-            message.range_min = 0.1f;
-            message.range_max = 40f;
-            message.ranges = new float[_scansPerRotation];
-        }
-
-        private void UpdateMessage()
-        {
-            // Update the header
-            message.header.Update();
-            Vector3 startingPosition = transform.position;
-            Vector3 direction = transform.forward;
-
-            // Update the starting angle
-            float offsetAngleDegrees = Vector3.SignedAngle(_initialAngle, direction, _rotationAxis);
-            float offsetAngleRadians = offsetAngleDegrees * Mathf.Deg2Rad;
-            message.angle_min = offsetAngleRadians;
-            message.angle_max = 6.28f - offsetAngleRadians;
-
-            // Scan the points and create the point cloud
-            float[] pointsCloud =_rotationScan.Scan(startingPosition, direction);
-            message.ranges = pointsCloud;
-
-            Publish(message);
+            transform.Rotate(0, RotationStep * 360 * Time.deltaTime, 0);
+            BoatDirection = transform.parent.forward;
         }
     }
 }
---
+++ Assets/Scripts/Sensors/LiDAR/Lidar2D/LiDAR2D.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RosSharp.RosBridgeClient
+{
+    public class LiDAR2D : LiDAR
+    {
+        private RotationScan _rotationScan;
+
+
+        // Start is called before the first frame update
+        protected override void Start()
+        {
+            base.Start();
+            _rotationScan = new RotationScan(ScansPerRotation, LaserLength);
+            InitializeMessage();
+            InvokeRepeating("UpdateMessage", 1f, 1f);
+        }
+
+    private void InitializeMessage()
+        {
+            Message = new MessageTypes.Sensor.LaserScan();
+            Message.header.frame_id = FrameId;
+            Message.angle_min = 0f;
+            Message.angle_max = 6.28f; // Full circle/rotation
+            Message.angle_increment = 6.28f / ScansPerRotation;
+            Message.time_increment = 0;
+            Message.scan_time = 0.1f;
+            Message.range_min = 0.1f;
+            Message.range_max = 40f;
+            Message.ranges = new float[ScansPerRotation];
+        }
+
+        private void UpdateMessage()
+        {
+            // Update the header
+            Message.header.Update();
+            Vector3 startingPosition = transform.position;
+            Vector3 direction = transform.forward;
+
+            // Update the starting angle
+            float offsetAngleDegrees = Vector3.SignedAngle(InitialAngle, direction, RotationAxis);
+            float offsetAngleRadians = offsetAngleDegrees * Mathf.Deg2Rad;
+            Message.angle_min = offsetAngleRadians;
+            Message.angle_max = 6.28f - offsetAngleRadians;
+
+            // Scan the points and create the point cloud
+            float[] pointsCloud =_rotationScan.Scan(startingPosition, direction);
+            Message.ranges = pointsCloud;
+
+            Publish(Message);
+        }
+    }
+}
