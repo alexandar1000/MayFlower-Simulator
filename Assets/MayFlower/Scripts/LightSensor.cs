8466866da90558a106ccd34e63b9419638224258
dennis9707 <he0707@hotmail.com>
2020-08-13 14:01:33 +0800

    added illuminanca publisher
    hardcoded lux figures

---
+++ Assets/MayFlower/Scripts/LightSensor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Runtime.Remoting.Messaging;
+using UnityEngine;
+
+namespace RosSharp.RosBridgeClient
+{
+    public class IntensityPublisher : UnityPublisher<MessageTypes.Sensor.Illuminance>
+    {
+        private MessageTypes.Sensor.Illuminance message;
+        public string FrameId = "Unity";
+
+        protected override void Start()
+        {
+            base.Start();
+            message = new MessageTypes.Sensor.Illuminance
+            {
+                header = new MessageTypes.Std.Header
+                {
+                    frame_id = FrameId
+                },
+                illuminance = 400,
+                variance = 0,
+            };
+            
+        }
+
+        void Update()
+        {
+
+            message.header.Update();
+            message.illuminance = 400;
+            message.variance = 0;
+
+            Publish(message);
+        }
+
+    }
+}
