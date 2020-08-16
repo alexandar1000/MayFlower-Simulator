24d76494d1135680f61dbebf8cc5f050dd73bfcf
Alexandar <alexandar1000@gmail.com>
2020-08-16 23:36:01 +0100

    Update IMU namespaces

--- Assets/MayFlower/Scripts/Sensors/IMU/IMUSensor.cs
+++ Assets/MayFlower/Scripts/Sensors/IMU/IMUSensor.cs
@@ -4,19 +4,20 @@ using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Runtime.Remoting.Messaging;
 using UnityEngine;
+using RosSharp.RosBridgeClient;
+using SensorMessages = RosSharp.RosBridgeClient.MessageTypes.Sensor;
+using GeometryMessages = RosSharp.RosBridgeClient.MessageTypes.Geometry;
+using MayflowerSimulator.Sensors.Compass;
 
-namespace RosSharp.RosBridgeClient
+namespace MayflowerSimulator.Sensors.IMU
 {
-    public class IMUSensor : UnityPublisher<MessageTypes.Sensor.Imu>
+    public class IMUSensor : UnityPublisher<SensorMessages.Imu>
     {
-        private MessageTypes.Sensor.Imu ImuMessage;
+        private SensorMessages.Imu ImuMessage;
         public string FrameId = "IMU_Sensor";
-       /* private static Quaternion MissionDirection;
-        private static Vector3 Accelerate_Linear;
-        private static Vector3 currentAngularVelocity;    */
         private double[] zeroArr = new double[9] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
-        void Start()
+        protected override void Start()
         {
             base.Start();
 
@@ -25,19 +26,19 @@ namespace RosSharp.RosBridgeClient
             InvokeRepeating("UpdateMessage", 1f, 1f);
         }
 
-        //create MessageTypes object
-        MessageTypes.Geometry.Quaternion quaterObj(Quaternion quaternion)
+        // Create MessageTypes object
+        GeometryMessages.Quaternion quaterObj(Quaternion quaternion)
         {
-            return new MessageTypes.Geometry.Quaternion(Math.Round(quaternion.x, 4), Math.Round(quaternion.y, 4), Math.Round(quaternion.z, 4), Math.Round(quaternion.w, 4));
+            return new GeometryMessages.Quaternion(Math.Round(quaternion.x, 4), Math.Round(quaternion.y, 4), Math.Round(quaternion.z, 4), Math.Round(quaternion.w, 4));
         }
-        MessageTypes.Geometry.Vector3 vector3Obj(Vector3 vector3)
+        GeometryMessages.Vector3 vector3Obj(Vector3 vector3)
         {
-            return new MessageTypes.Geometry.Vector3(Math.Round(vector3.x, 4), Math.Round(vector3.y, 4), Math.Round(vector3.z, 4));
+            return new GeometryMessages.Vector3(Math.Round(vector3.x, 4), Math.Round(vector3.y, 4), Math.Round(vector3.z, 4));
         }
 
         void InitialiseMessage()
         {
-            ImuMessage = new MessageTypes.Sensor.Imu();
+            ImuMessage = new SensorMessages.Imu();
             ImuMessage.header.frame_id = FrameId;
             ImuMessage.orientation = quaterObj(CompassSensor.MissionDirection);
             ImuMessage.linear_acceleration = vector3Obj(IMU.Accelerate_Linear);
@@ -54,13 +55,7 @@ namespace RosSharp.RosBridgeClient
             ImuMessage.orientation = quaterObj(CompassSensor.MissionDirection);
             ImuMessage.linear_acceleration = vector3Obj(IMU.Accelerate_Linear);
             ImuMessage.angular_velocity = vector3Obj(IMU.currentAngularVelocity);
-            //UnityEngine.Debug.Log("IMU Message: " + ImuMessage.linear_acceleration.x + ", " + ImuMessage.linear_acceleration.y + ", " + ImuMessage.linear_acceleration.z + ")"); 
             Publish(ImuMessage);            
         }
-
-       /* private MessageTypes.Sensor.Imu PrepareMessage(MessageTypes.Sensor.Imu message)
-        { 
-            return message;
-        }*/
     }
 }
