2d4448cf2f59f99e50367438b4aa748612348307
dennis9707 <he0707@hotmail.com>
2020-08-20 15:41:15 +0800

    added light script

---
+++ Assets/MayFlower/Scripts/Sensors/Light/Light.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MayflowerSimulator.Sensors.Light
+{
+    public class Light : MonoBehaviour
+    {
+        public bool isSwitchOn;
+    }
+}
